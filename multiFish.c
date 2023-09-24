e5dc62b6fecdfb057f652136044bc115b51fc77f
Wolfgang1208 <Wolfgang1208@outlook.com>
2023-09-24 11:40:10 +0800

    update

--- multiFish.c
+++ multiFish.c
@@ -6,7 +6,7 @@
 
 // this is about 10^7 fishes
 // #define MAX_FISH_10_7 10000000
-#define MAX_FISH 10000000
+#define MAX_FISH 4096
 #define MAX_FISH_INIT_WEIGHT 10
 #define MAX_FISH_X 100
 #define MAX_FISH_Y 100
@@ -16,7 +16,7 @@
 
 #define GEN_NUM 10
 
-#define MAX_THREAD 20
+#define MAX_THREAD 16
 
 struct fish
 {
@@ -24,118 +24,162 @@ struct fish
     float y;
     float w;
     float max_w;
-    float lastDelta;
+    int isSwam;
+    float ED;
+    float lastOBJ;
 };
+
+float currED[MAX_FISH];
+
+float maxGlobalOBJ = -1.0;
+
+int countFatFishes = 0;
 
 void fishInit(struct fish *fishes, int fishNum)
 {
-    // fish parameters should have one decimal fraction
-#pragma omp parallel
-#pragma omp for schedule(dynamic, 100000)
+// fish parameters should have one decimal fraction
+#pragma omp parallel for schedule(dynamic)
     for (int i = 0; i < fishNum; i++)
     {
         fishes[i].x = (rand() % (MAX_FISH_X * 10 - MIN_FISH_X * 10 + 1) + MIN_FISH_X * 10) / 10.0;
         fishes[i].y = (rand() % (MAX_FISH_Y * 10 - MIN_FISH_Y * 10 + 1) + MIN_FISH_Y * 10) / 10.0;
         fishes[i].w = (rand() % (MAX_FISH_INIT_WEIGHT * 10 - MIN_FISH_WEIGHT * 10 + 1) + MIN_FISH_WEIGHT * 10) / 10.0;
         fishes[i].max_w = fishes[i].w * 2;
-        fishes[i].lastDelta = 0.0;
+        fishes[i].ED = 0.0;
+        fishes[i].isSwam = 0;
+        fishes[i].lastOBJ = 0.0;
+        currED[i] = sqrt(pow(fishes[i].x, 2) + pow(fishes[i].y, 2));
     }
 }
 
-void fishesSwim(struct fish *fishes, int fishNum)
+void fishEat(struct fish *fishes, int currFish, int fishNum)
 {
-#pragma omp parallel
-#pragma omp for schedule(dynamic, 100000)
+// calculate curr fish's ED
+#pragma omp parallel for schedule(dynamic)
+#pragma omp reduction(+ : currED)
     for (int i = 0; i < fishNum; i++)
     {
-        float curFishX = fishes[i].x;
-        float curFishY = fishes[i].y;
-        // get a value between -0.1 and 0.1 for x changes
-        float xChange = (rand() % 21 - 10) / 100.0;
-
-        // update fish x
-        if (curFishX + xChange > MAX_FISH_X)
+        if (fishes[i].isSwam == 1)
         {
-            fishes[i].x = MAX_FISH_X;
+            currED[i] = sqrt(pow(fishes[i].x, 2) + pow(fishes[i].y, 2));
         }
-        else if (curFishX + xChange < MIN_FISH_X)
+    }
+
+    // curr fish's obj
+    float currOBJ = 0.0;
+    for (int i = 0; i < fishNum; i++)
+    {
+        currOBJ += currED[i];
+    }
+
+    // curr fish's delta obj
+    float deltaOBJ = currOBJ - fishes[currFish].lastOBJ;
+
+    fishes[currFish].lastOBJ = currOBJ;
+
+    // change global max obj if needed
+    if (deltaOBJ >= maxGlobalOBJ)
+    {
+        maxGlobalOBJ = deltaOBJ;
+    }
+
+    // set new weight
+    float newWeight = fishes[currFish].w + deltaOBJ / maxGlobalOBJ;
+    if (newWeight > fishes[currFish].max_w)
+    {
+        fishes[currFish].w = fishes[currFish].max_w;
+        countFatFishes += 1;
+    }
+    else if (newWeight < MIN_FISH_WEIGHT)
+    {
+        fishes[currFish].w = MIN_FISH_WEIGHT;
+    }
+    else
+    {
+        fishes[currFish].w = newWeight;
+    }
+}
+
+void fishSwim(struct fish *fishes, int currFish, int fishNum)
+{
+
+    float currFishX = fishes[currFish].x;
+    float currFishY = fishes[currFish].y;
+    // get a value between -0.1 and 0.1 for x changes
+    float xChange = (rand() % 21 - 10) / 100.0;
+
+    if (fishes[currFish].w < fishes[currFish].max_w)
+    {
+        // update fish x
+        if (currFishX + xChange > MAX_FISH_X)
         {
-            fishes[i].x = MIN_FISH_X;
+            fishes[currFish].x = MAX_FISH_X;
+        }
+        else if (currFishX + xChange < MIN_FISH_X)
+        {
+            fishes[currFish].x = MIN_FISH_X;
         }
         else
         {
-            fishes[i].x += xChange;
+            fishes[currFish].x += xChange;
         }
 
         // get a value between -0.1 and 0.1 for y changes
         float yChange = (rand() % 21 - 10) / 100.0;
 
         // update fish y
-        if (curFishY + yChange > MAX_FISH_Y)
+        if (currFishY + yChange > MAX_FISH_Y)
         {
-            fishes[i].y = MAX_FISH_Y;
+            fishes[currFish].y = MAX_FISH_Y;
         }
-        else if (curFishY + yChange < MIN_FISH_Y)
+        else if (currFishY + yChange < MIN_FISH_Y)
         {
-            fishes[i].y = MIN_FISH_Y;
+            fishes[currFish].y = MIN_FISH_Y;
         }
         else
         {
-            fishes[i].y += yChange;
-        }
-    }
-}
-
-void fishesEat(struct fish *fishes, int fishNum)
-{
-    // table based optimization
-    float *fishesDistance = (float *)malloc(sizeof(float) * fishNum);
-    float *fishesWeight = (float *)malloc(sizeof(float) * fishNum);
-    float *fishesDelta = (float *)malloc(sizeof(float) * fishNum);
-
-// calculate fishesDistance
-#pragma omp parallel
-#pragma omp for schedule(dynamic, 100000)
-    for (int i = 0; i < fishNum; i++)
-    {
-        fishesDistance[i] = sqrt(pow(fishes[i].x, 2) + pow(fishes[i].y, 2));
-    }
-
-// calculate fishesWeight
-#pragma omp parallel
-#pragma omp for schedule(dynamic, 100000)
-    for (int i = 0; i < fishNum; i++)
-    {
-        fishesWeight[i] = fishes[i].w;
-    }
-
-// calculate fishesDelta
-#pragma omp parallel
-#pragma omp for schedule(dynamic, 100000)
-    for (int i = 0; i < fishNum; i++)
-    {
-        fishesDelta[i] = fishesDistance[i] - fishes[i].lastDelta;
-    }
-
-    // find maximum delta
-    float maxDelta = 0.0;
-
-    for (int i = 0; i < fishNum; i++)
-    {
-        if (fishesDelta[i] > maxDelta)
-        {
-            maxDelta = fishesDelta[i];
+            fishes[currFish].y += yChange;
         }
     }
 
-// calculate fishesWeight
-#pragma omp parallel
-#pragma omp for schedule(dynamic, 100000)
-    for (int i = 0; i < fishNum; i++)
+    if (currFishX != fishes[currFish].x || currFishY != fishes[currFish].y)
     {
-        fishes[i].w = fishesWeight[i] + fishesDelta[i] / maxDelta;
+        fishes[currFish].isSwam = 1;
+    }
+    else
+    {
+        fishes[currFish].isSwam = 0;
     }
 }
+
+// void fishEat(struct fish *fishes, int fishNum)
+// {
+// #pragma omp parallel
+// #pragma omp for schedule(dynamic, 100000)
+//     for (int i = 0; i < fishNum; i++)
+//     {
+//         float f = 0.0;
+// #pragma omp schedule(dynamic)
+//         for (int j = 0; j <= i; j++)
+//         {
+//             f += sqrt(pow(fishes[j].x, 2) + pow(fishes[j].y, 2));
+//         }
+//         fishes[i].lastDelta = f - fishes[i].lastDelta;
+//         // printf("Fish %d: %.5f\n", i, fishes[i].lastDelta);
+
+//         float max_df = 0.0;
+//         for (int m = 0; m < fishNum; m++)
+//         {
+//             if (fishes[m].lastDelta >= max_df)
+//             {
+//                 max_df = fishes[m].lastDelta;
+//             }
+//         }
+//         printf("Fish %d max_df: %.5f\n", i, max_df);
+//         printf("Fish %d: %.5f\n", i, (fishes[i].lastDelta) / (max_df));
+//         fishes[i].w += (fishes[i].lastDelta) / (max_df);
+//     }
+// }
 
 float bariUpdate(struct fish *fishes, int fishNum)
 {
@@ -159,16 +203,45 @@ void main()
 
     fishes = (struct fish *)malloc(sizeof(struct fish) * fishNum);
 
-    fishInit(fishes, fishNum);
-
     float Bari[GEN_NUM];
 
-    // Swim for 10 times
+    // init currED
+    for (int i = 0; i < fishNum; i++)
+    {
+        currED[i] = 0.0;
+    }
+
+    fishInit(fishes, fishNum);
+
+    // Iterate for GEN_NUM times
     for (int i = 0; i < GEN_NUM; i++)
     {
-        fishesSwim(fishes, fishNum);
-        fishesEat(fishes, fishNum);
+        double start = omp_get_wtime();
+        // reset status all of the fishes
+#pragma parallel for schedule(dynamic)
+        for (int j = 0; j < fishNum; j++)
+        {
+            fishes[j].isSwam = 0;
+        }
+
+        // parallel for each chrunk of fishes
+#pragma parallel
+#pragma for schedule(dynamic)
+#pragma reduction(+ : currED, maxGlobalOBJ, countFatFishes)
+        for (int currFish = 0; currFish < MAX_FISH; currFish++)
+        {
+            fishEat(fishes, currFish, fishNum);
+            fishSwim(fishes, currFish, fishNum);
+        }
+#pragma parallel barrier
+
+        printf("There are %d fat fishes.\n", countFatFishes);
+
+        countFatFishes = 0;
+
+        double end = omp_get_wtime();
+        printf("Time: %f seconds. \n", end - start);
         Bari[i] = bariUpdate(fishes, fishNum);
-        printf("Bari %i: %.5f\n", i, Bari[i]);
+        printf("Bari: %.5f\n", Bari[i]);
     }
 }
